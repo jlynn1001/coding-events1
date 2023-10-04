a7c1cfe6839e14bd1a44bb946d5620d1e4d64223
jlynn1001 <121581602+jlynn1001@users.noreply.github.com>
2023-10-04 18:16:36 -0500

    Add files via upload

---
+++ coding-events1/src/main/java/org/launchcode/codingevents1/CodingEvents1Application.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingevents1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingEvents1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingEvents1Application.class, args);
+	}
+
+}
---
+++ coding-events1/src/test/java/org/launchcode/codingevents1/CodingEvents1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingevents1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEvents1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
